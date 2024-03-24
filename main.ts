6ec63022c5d767d9a4f917824a4e67168f12c24d
microdirk <164635705+microdirk@users.noreply.github.com>
2024-03-24 13:06:50 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let reading = 0
 music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+led.setBrightness(6)
 basic.forever(function () {
     reading = pins.analogReadPin(AnalogPin.P1)
     led.plotBarGraph(
@@ -12,8 +13,9 @@ basic.forever(function () {
     if (reading < 500) {
         music.playTone(262, music.beat(BeatFraction.Whole))
         pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(500)
+        basic.pause(700)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
+    pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(1000)
 })
