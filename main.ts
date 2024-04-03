ab2f927db67633df7503ace8eb56198ec58ac2f8
microdirk <164635705+microdirk@users.noreply.github.com>
2024-04-03 18:50:14 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
     }
-    if (reading < 650) {
+    if (reading < 680) {
         pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(850)
         pins.digitalWritePin(DigitalPin.P2, 0)
