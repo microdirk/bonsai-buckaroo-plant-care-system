9aeda66bdf2aeb5f7ff87694b82a9c1dd48919d1
microdirk <164635705+microdirk@users.noreply.github.com>
2024-04-22 17:11:08 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,28 @@ music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
 led.setBrightness(6)
 basic.forever(function () {
     reading = pins.analogReadPin(AnalogPin.P1)
-    led.plotBarGraph(
-    reading,
-    1023
-    )
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(reading)
     }
-    if (reading < 540) {
+    if (reading < 350) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
         pins.digitalWritePin(DigitalPin.P2, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P2, 0)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
     }
     pins.digitalWritePin(DigitalPin.P1, 0)
     basic.pause(5000)
